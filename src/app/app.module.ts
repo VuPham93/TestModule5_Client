119a28c06f47b868cef0b263547625ec780c3ccb
VuPham93 <62596412+VuPham93@users.noreply.github.com>
2020-08-24 10:32:39 +0700

    Add Client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookAddComponent } from './book-add/book-add.component';
+import { BookDeleteComponent } from './book-delete/book-delete.component';
+import { BookDetailComponent } from './book-detail/book-detail.component';
+import { BookEditComponent } from './book-edit/book-edit.component';
+import { BookListComponent } from './book-list/book-list.component';
+import {HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookAddComponent,
+    BookDeleteComponent,
+    BookDetailComponent,
+    BookEditComponent,
+    BookListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
